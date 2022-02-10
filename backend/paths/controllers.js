0d5ad0039ab1ec00aa2fb5f2069225c72b6009a8
shobhit <shobhitmishra2002@gmail.com>
2022-02-10 18:37:17 +0530

    added the authenticating sys

--- backend/paths/controllers.js
+++ backend/paths/controllers.js
@@ -5,3 +5,22 @@ export const puttingUser = async (req, res) => {
   const data = await UserModel.create(req.body);
   res.send(data);
 };
+
+//this will get all the users in the database
+export const getAllUsers = async (req, res) => {
+  const data = await UserModel.find({}, { __v: 0 }).exec();
+  res.send(data);
+};
+
+//this will authenticate the user
+export const authUser = async (req, res) => {
+  const data = await UserModel.findOne({
+    email: req.params.email,
+    passwd: req.params.passwd,
+  });
+  if (data) {
+    res.send({ validation: 1 });
+  } else {
+    res.send({ validation: 0 });
+  }
+};
--- backend/paths/router.js
+++ backend/paths/router.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
 import { UserModel } from "../model/model.schema";
-import { puttingUser } from "./controllers";
+import { puttingUser, getAllUsers, authUser } from "./controllers";
 
 const router = Router();
 
 //POST: {UserName, passwd, email}
 router.route("/getUser").post(puttingUser);
+
+//to get all users
+router.route("/getAllUsers").get(getAllUsers);
+
+//to authenticate the user
+router.route("/auth/:email/:passwd").get(authUser);
 
 export default router;
